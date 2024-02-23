012b2ba8a95b285fa620bceceb4eb375165e0a08
Courtney Lawson <courtneyalawson@yahoo.com>
2024-02-22 20:53:56 -0500

    added api calls

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
-const express = require('express')
-const app = express()
-app.use(express.json())
-const port = 3000
-app.listen(port, () => {
-  console.log(` server listening on port ${port} `)
+//dependencies <- packages
+const express = require('express');
+
+//imports <- other files in project
+
+// configuration
+const app = express();
+app.use(express.json()); //<-- for consistency
+const port = 3000; // <- enventually in env for safety reasons
+
+// API calls
+app.get('/hello', (req, res) => {
+    res.send('Hello World');
+});
+
+app.get('/status', (req, res) => {
+    res.send('The port is 3000');
+});
+
+app.get('/error', (req, res) => {
+    res.send('Oops! We hit a snag!')
+});
+
+app.get ('/email-list/:email', (req, res) => {
+    res.send(req.params.email);
+});
+// feedback
+app.listen(3000, () => {
+  console.log(` server listening on port ${3000} `)
 })
